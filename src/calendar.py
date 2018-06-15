75c5e8cddb4407eb043fc11ec099442e9fc0b059
alexandrabenamar <alexandra.benamar@yahoo.Fr>
2018-06-15 14:28:55 +0200

    add calendar updates

--- src/calendar.py
+++ src/calendar.py
@@ -1,3 +1,5 @@
+##!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 from googleapiclient.discovery import build
@@ -45,23 +47,48 @@ def getUpcomingEvents(service, number_of_events, timeMin):
         print(start, event['summary'])
     return events
 
-def createEvent(service, event):
+def addEvent(service, event):
     """
+        Adding an event to your calendar.
     """
     event = service.events().insert(calendarId='primary', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
+
+def updateEventTitle(service, eventId, summary):#, NEW_EVENT):
+    """
+        Update the name of an existing event.
+    """
+    event = service.events().get(calendarId='primary', eventId=eventId).execute()
+    event['summary'] = summary
+    updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+    print(updated_event['updated'])
+
+
+def updateEventDate(service, eventId, startDate, endDate):
+    """
+        Update the date of an existing event.
+    """
+    event = service.events().get(calendarId='primary', eventId=eventId).execute()
+    event['start']['dateTime'] = startDate
+    event['end']['dateTime'] = endDate
+    updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+    print(updated_event['updated'])
+    print('Event updated: %s' % (event.get('htmlLink')))
+
 
 if __name__ == '__main__':
     SCOPES = 'https://www.googleapis.com/auth/calendar'
     credentials = getCredentials('../credentials/client_secret.json')
     service = build('calendar', 'v3', http=credentials.authorize(Http()))
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events = getUpcomingEvents(service, 10, now)
-    GMT_OFF = '-07:00'    # PDT/MST/GMT-7
-    EVENT = {
-        'summary': 'Dinner with friends',
-        'start':   {'dateTime': '2015-09-15T19:00:00%s' % GMT_OFF},
-        'end':     {'dateTime': '2015-09-15T22:00:00%s' % GMT_OFF},
+    #events = getUpcomingEvents(service, 10, now)
+    GMT_OFF = '+02:00'
+    event = {
+        'id': 'testconc',
+        'summary': 'Concert à Bercy',
+        'start':   {'dateTime': '2018-07-02T04:00:00%s' % GMT_OFF},
+        'end':     {'dateTime': '2018-07-02T09:00:00%s' % GMT_OFF},
     }
-    createEvent(service, EVENT)
-    print()
+    addEvent(service, event)
+    updateEventTitle(service, 'testconc', 'Concert à ciel ouvert')
+    updateEventDate(service, 'testconc', '2018-07-02T08:00:00+02:00', '2018-07-02T10:00:00+02:00')
