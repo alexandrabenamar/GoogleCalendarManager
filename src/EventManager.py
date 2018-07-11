110f437517658ede7cf1b77fd4f6d9d2cd94f14c
alexandrabenamar <alexandra.benamar@yahoo.Fr>
2018-07-11 12:24:20 +0200

    modification mineure

--- src/EventManager.py
+++ src/EventManager.py
@@ -41,7 +41,7 @@ def getUpcomingEvents(service, number_of_events, timeMin, calendarId='primary'):
                                       maxResults=number_of_events, singleEvents=True,
                                       orderBy='startTime', showDeleted=True).execute()
     events = events_result.get('items', [])
-    print(len(events), " events were found in your calendar : ", calendarId)
+    print(len(events), " events were found in : ", calendarId)
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
@@ -90,15 +90,5 @@ if __name__ == '__main__':
     SCOPES = 'https://www.googleapis.com/auth/calendar'
     credentials = getCredentials('../credentials/client_secret.json')
     service = build('calendar', 'v3', http=credentials.authorize(Http()))
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events = getUpcomingEvents(service, 10, now)
-    GMT_OFF = '+02:00'
-    event = {
-        'id': 'testcon',
-        'summary': 'Concert à Bercy',
-        'start':   {'dateTime': '2018-07-02T04:00:00%s' % GMT_OFF},
-        'end':     {'dateTime': '2018-07-02T09:00:00%s' % GMT_OFF},
-    }
-    addEvent(service, event)
-    updateEventTitle(service, 'testcon', 'Concert à ciel ouvert')
-    updateEventDate(service, 'testcon', '2018-07-02T08:00:00+02:00', '2018-07-02T10:00:00+02:00')
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    #perform actions
